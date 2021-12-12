6c0b480a13bdca411e022f5f057fbd7159d45243
josinhr <josinhr@gmail.com>
2021-12-12 18:35:43 +0100

    added validation

--- app/routes/blogEntries/newEntry.tsx
+++ app/routes/blogEntries/newEntry.tsx
@@ -1,23 +1,130 @@
+import { ActionFunction, json, redirect, useActionData } from "remix";
+import { db } from "~/utils/db.server";
+import { ActionData } from "~/utils/types";
+
+function validateEntryTitle(title: string): string | undefined {
+  if (title.length < 3) return "Title must be at least 3 characters long";
+  return undefined;
+}
+
+function validateEntryContent(content: string): string | undefined {
+  if (content.length < 10) return "Content must be at least 10 characters long";
+  return undefined;
+}
+
+function validateEntrySubtitle(subtitle: string): string | undefined {
+  if (subtitle.length < 3) return "Subtitle must be at least 3 characters long";
+  return undefined;
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const subtitle = form.get("subtitle");
+  const content = form.get("content");
+
+  if (
+    typeof title !== "string" ||
+    typeof content !== "string" ||
+    typeof subtitle !== "string"
+  ) {
+    return badRequest({
+      formError: `Form not submitted correctly.`,
+    });
+  }
+
+  const fieldErrors = {
+    title: validateEntryTitle(title),
+    subtitle: validateEntrySubtitle(subtitle),
+    content: validateEntryContent(content),
+  };
+  const fields = { title, content, subtitle };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
+
+  const entry = await db.entry.create({ data: fields });
+  return redirect(`/blogEntries/${entry.id}`);
+};
+
 export default function NewEntry() {
+  const actionData = useActionData<ActionData>();
+
   return (
     <div>
-        <p>Add your new blog entry</p>
-        <form  method="post">
-            <div>
-                <label > Title: <input placeholder="Your title here" type="text" name="title" id="title"  /></label>
-            </div>
-            <div>
-                <label > Subtitle: <input placeholder="Your subtitle here" type="text" name="subtitle" id="subtitle"  /></label>
-            </div>
-            <div>
-                <label > Content: <textarea name="entryContent" id="entryContent" ></textarea></label>
-            </div>
+      <p>Add your new blog entry</p>
+      <form method="post">
+        <div>
+          <label>
+            {" "}
+            Title:{" "}
+            <input
+              placeholder="Your title here"
+              defaultValue={actionData?.fields?.title}
+              type="text"
+              name="title"
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.title) || undefined
+              }
+              aria-describedby={
+                actionData?.fieldErrors?.title ? "name-error" : undefined
+              }
+              id="title"
+            />
+          </label>
+          {actionData?.fieldErrors?.title ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.title}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          <label>
+            {" "}
+            Subtitle:{" "}
+            <input
+              placeholder="Your subtitle here"
+              defaultValue={actionData?.fields?.subtitle}
+              type="text"
+              name="subtitle"
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.subtitle) || undefined
+              }
+              aria-describedby={
+                actionData?.fieldErrors?.subtitle ? "name-error" : undefined
+              }
+              id="subtitle"
+            />
+          </label>
+          {actionData?.fieldErrors?.subtitle ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.subtitle}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          <label>
+            {" "}
+            Content:{" "}
+            <textarea
+              name="content"
+              id="content"
+              cols={80}
+              rows={10}
+            ></textarea>
+          </label>
+        </div>
 
-            <div>
-                <button type="submit" className="button"> Add</button>
-            </div>
-        </form>
+        <div>
+          <button type="submit" className="button">
+            {" "}
+            Add
+          </button>
+        </div>
+      </form>
     </div>
   );
-
 }
--- app/utils/types.ts
+++ app/utils/types.ts
@@ -7,3 +7,17 @@ export type BlogEntry = {
 export type BlogEntryFromDatabase = BlogEntry & {
   id: string;
 };
+
+export type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    title: string | undefined;
+    subtitle: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    title: string;
+    subtitle: string;
+    content: string;
+  };
+};
